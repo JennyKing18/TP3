ea6f798c431b40be1d7c1ebc1498daa252e7986b
Jenny King <jen.kin.bus@gmail.com>
2023-06-02 10:25:33 -0600

    Inicio

---
+++ pruebas/pruebasJenny.py
@@ -0,0 +1,9 @@
+###########################################################
+#Elaborado por Jenny King Josue Salazar
+#Fecha de creacion 02/06/23 10:30am
+#Ultima modificacion 02/06/23 10:30am
+#version 3.10.6
+###########################################################
+#importacion de librerias
+
+#definicion de funciones
