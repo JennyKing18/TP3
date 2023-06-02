8ef7284b44f6217498a4a5c9fc8c488b4f6f0512
Jenny King <jen.kin.bus@gmail.com>
2023-06-02 12:58:46 -0600

    pruebas de web scrapping

--- pruebas/pruebasJenny.py
+++ pruebas/pruebasJenny.py
@@ -5,5 +5,32 @@
 #version 3.10.6
 ###########################################################
 #importacion de librerias
-
+from bs4 import BeautifulSoup
+import requests
+import re
 #definicion de funciones
+website= 'https://practicatest.cr/blog/licencias/tipos-licencia-conducir-costa-rica'
+resultado= requests.get(website)
+contenido= resultado.text
+
+soup=BeautifulSoup(contenido,'html.parser')
+tipoLicencia= soup.find_all('h2')
+
+print(type(tipoLicencia))
+patronST=r'Licencia [A-Z]\d+'
+subtipo=soup.find_all('h3',string=re.compile(patronST))
+
+
+for st in subtipo:
+    comentario = st.find_next_sibling('p')
+    requisitos_header = comentario.find_next_sibling('h4')
+    requisitos = requisitos_header.find_next_sibling('ul').find_all('li')
+    
+    print(st.text.strip())
+    print(comentario.text.strip())
+    print('Requisitos:')
+    for requisito in requisitos:
+        print('- ' + requisito.text.strip())
+    
+    print('-' * 30)
+
