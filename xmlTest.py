ab4b7b4ad8a1d51f58996e9e922c9b1e40eacc31
Jenny King <jen.kin.bus@gmail.com>
2023-06-04 17:06:52 -0600

    purging, cleaning

--- xmlTest.py
+++ xmlTest.py
@@ -22,57 +22,43 @@ tipoLicencia= soup.find_all('h2')
 patronST=r'Licencia [A-Z]\d+'
 subtipo=soup.find_all('h3',string=re.compile(patronST))
 
-# XML
-raiz=ET.Element('Tipos de Licencias')
-#
-
-for tipo in tipoLicencia:
-    # consigue el titulo 
-    titulo = tipo.text.strip()
-    subElemento=ET.SubElement(raiz, titulo)
-    print(f'\033[1;30;44m {titulo} \033[0;0m')
-    
-    ListaSubtipo = []
-    listaComentarios = []
-    listaReque = []
-
-    # Encuentra todos los datos dentro del 'h2' tag
-    next_sibling = tipo.next_sibling
-    while next_sibling and next_sibling.name != 'h2':
-        if next_sibling.name == 'h3':
-            # extrae subtipo,comentario,requerimientos 
-            subTipo = next_sibling.text.strip()
-            ListaSubtipo.append(subTipo)
-            comentario = next_sibling.find_next_sibling('p')
-            comentario=comentario.text.strip()
-            listaComentarios.append(comentario)
-            #print(type(subTipo),type(comentario))
-
-            subTipos= ET.SubElement(subElemento,subTipo)
-            subTipos.text=comentario
-
-            if re.findall(r'Licencia D\d+', subTipo):
-                listaReque = 'Cédula o documento de identificación.\nDictamen médico digital para licencia.\nAprobar el curso teórico básico para licencia.'
-                
-                listaReque = [listaReque] * len(ListaSubtipo)
-                #listaReque.append([listaReque])
-                subTipos.text=str(listaReque)
-            else:
-                requerimientos = next_sibling.find_next_sibling('ul')
-                requerimientos=requerimientos.text.strip()
-                listaReque.append(requerimientos)
-                subTipos.text=str(listaReque)
-        
-        next_sibling = next_sibling.next_sibling
-    for subTipo, comentario, requerimientos in zip(ListaSubtipo, listaComentarios, listaReque):
+def crearXML():
+    raiz=ET.Element('Tipos de Licencias')
+    for tipo in tipoLicencia:
+        # consigue el titulo 
+        titulo = tipo.text.strip()
+        subElemento=ET.SubElement(raiz, titulo)
+        print(f'\033[1;30;44m {titulo} \033[0;0m')
+        listaSubtipo,listaComentarios,listaReque= [],[],[]
+        # Encuentra todos los datos dentro del 'h2' tag
+        next_sibling = tipo.next_sibling
+        while next_sibling and next_sibling.name != 'h2':
+            if next_sibling.name == 'h3':
+                # extrae subtipo,comentario,requerimientos 
+                subTipo = next_sibling.text.strip()
+                listaSubtipo.append(subTipo)
+                comentario = next_sibling.find_next_sibling('p')
+                comentario=comentario.text.strip()
+                listaComentarios.append(comentario)
+                subTipos= ET.SubElement(subElemento,subTipo)
+                subTipos.text=comentario
+                if re.findall(r'Licencia D\d+', subTipo):
+                    listaReque = 'Cédula o documento de identificación.\nDictamen médico digital para licencia.\nAprobar el curso teórico básico para licencia.'
+                    listaReque = [listaReque] * len(listaSubtipo)
+                    subTipos.text=str(listaReque)
+                else:
+                    requerimientos = next_sibling.find_next_sibling('ul')
+                    requerimientos=requerimientos.text.strip()
+                    listaReque.append(requerimientos)
+                    subTipos.text=str(listaReque)
+            next_sibling = next_sibling.next_sibling
+        for subTipo, comentario, requerimientos in zip(listaSubtipo, listaComentarios, listaReque):
+            print('-'.center(100,'-'))
+            print(subTipo+'\n'+comentario+'\n''\nRequerimientos:\n'+requerimientos+'\n')
         print('-'.center(100,'-'))
-        print(subTipo)
-        print(comentario,'\n')
-        print('Requerimientos:\n'+requerimientos)
-        print()
-
-    print('-'.center(100,'-'))
-tree=ET.ElementTree(raiz)
-tree.write(str('informacion.xml'))
-    #grabar('informacion.xml',tree)
+    tree=ET.ElementTree(raiz)
+    tree.write(str('informacion.xml'))
+    return ''
+print(crearXML())
+#grabar('informacion.xml',tree)
 #print(raiz)
